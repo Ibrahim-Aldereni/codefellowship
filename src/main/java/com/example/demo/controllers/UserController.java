69b988add6572f3806900bbef8ff34a3a4012a20
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-08-16 13:51:23 +0300

    finish lab17

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controllers;
 
 import com.example.demo.modals.ApplicationUser;
+import com.example.demo.modals.Post;
 import com.example.demo.repositories.ApplicationUserRepository;
+import com.example.demo.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
 
@@ -14,26 +19,27 @@ import java.security.Principal;
 public class UserController {
     @Autowired
     ApplicationUserRepository applicationUserRepository;
+    @Autowired
+    PostRepository postRepository;
 
-    @GetMapping("/user/{id}")
-    public String getUser(Principal p, Model model, @PathVariable Integer id){
-
+    @GetMapping("/myprofile")
+    public String getUserProfile(Principal p, Model model){
         try{
             // for the header
             model.addAttribute("userInfo",p.getName());
-            model.addAttribute("hideWhenLogin","display: none");
-            model.addAttribute("hideWhenLogout","display: auto");
-
             // for the body
-            model.addAttribute("allUserInfo",applicationUserRepository.findById(id).get());
+            model.addAttribute("allUserInfo",applicationUserRepository.findByUsername(p.getName()));
         }catch (NullPointerException e){
             model.addAttribute("userInfo","");
-            model.addAttribute("hideWhenLogin","display: auto");
-            model.addAttribute("hideWhenLogout","display: none");
             model.addAttribute("allUserInfo",new ApplicationUser());
         }
         return "user.html";
     }
 
-
+    @PostMapping("/myprofile")
+    public RedirectView addPost(Principal p,@RequestParam String body){
+        Post post = new Post(body,applicationUserRepository.findByUsername(p.getName()));
+        postRepository.save(post);
+        return new RedirectView("/myprofile");
+    }
 }
